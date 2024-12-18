a050adc7445a70ad4b9a497d3054394f6c5d7771
Alexander Kh <av.kharin@gmail.com>
2024-12-18 13:29:20 +0500

    Implement new HTTP Method:  "/ad" http.MethodGet

--- internal/ads/service.go
+++ internal/ads/service.go
@@ -4,6 +4,7 @@ type Storage interface {
 	CreateAd(title, description string, price float64) (int, error)
 	GetAllAd() ([]Ad, error)
 	UpdateAd(ad Ad) (bool, error)
+	GetAd(id int) (*Ad, error)
 	// TODO: add other CRUD methods
 }
 
@@ -22,18 +23,6 @@ func NewService(store Storage) *Service {
 	return &Service{store: store}
 }
 
-func (s *Service) UpdateAd(ad Ad) (bool, error) {
-	return s.store.UpdateAd(ad)
-}
-
-func (s *Service) GetAllAds() ([]Ad, error) {
-	ads, err := s.store.GetAllAd()
-	if err != nil {
-		return []Ad{}, err
-	}
-	return ads, nil
-}
-
 func (s *Service) CreateAd(title, description string, price float64) (*Ad, error) {
 	id, err := s.store.CreateAd(title, description, price)
 	if err != nil {
@@ -45,6 +34,22 @@ func (s *Service) CreateAd(title, description string, price float64) (*Ad, error
 		Description: description,
 		Price:       price,
 	}, nil
+}
+
+func (s *Service) GetAd(id int) (*Ad, error) {
+	return s.store.GetAd(id)
+}
+
+func (s *Service) GetAllAds() ([]Ad, error) {
+	ads, err := s.store.GetAllAd()
+	if err != nil {
+		return []Ad{}, err
+	}
+	return ads, nil
+}
+
+func (s *Service) UpdateAd(ad Ad) (bool, error) {
+	return s.store.UpdateAd(ad)
 }
 
 // TODO: implement other CRUD methods
